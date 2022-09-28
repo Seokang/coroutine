e2989b26f0d658daf3b57bf712cd2eb557a2a1ed
SeoKang <seokang@dnx.kr>
2022-09-28 16:38:39 +0900

    Base Coroutine

--- app/src/main/java/me/seokang/coroutine/MainActivity.kt
+++ app/src/main/java/me/seokang/coroutine/MainActivity.kt
@@ -1,11 +1,40 @@
 package me.seokang.coroutine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        blocking()
+
+        Log.d(TAG, "onCreate: ")
+    }
+
+    private fun blocking() = runBlocking {
+        launch {
+            Log.d(TAG, "main: ${delayMethod(10_000L)}")
+        }
+
+        withContext(Dispatchers.Default) {
+            Log.d(TAG, "main: ${delayMethod(5_000L)}")
+        }
+
+        Log.d(TAG, "main: finish")
+    }
+
+    private fun beforeDelay(): String {
+        return "before delay"
+    }
+    private suspend fun delayMethod(value: Long): String {
+        delay(value)
+        return "delay-${value} finish"
     }
 }
